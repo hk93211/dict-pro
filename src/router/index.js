3eb949730d27fa893df05f642ba76f340458eb8c
黄鹍 <352660431@qq.com>
2019-11-25 10:50:15 +0800

    feat: uptimize side menu display

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,5 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
-import Home1 from '../views/Home1.vue';
 import expense from './expense/index';
 
 Vue.use(VueRouter);
@@ -9,16 +7,6 @@ Vue.use(VueRouter);
 console.log(expense);
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home,
-  },
-  {
-    path: '123',
-    name: '123',
-    component: Home1
-  },
   ...expense,
 ];
 
